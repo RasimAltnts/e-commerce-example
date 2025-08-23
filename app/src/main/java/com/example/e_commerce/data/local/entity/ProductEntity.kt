41f982d72b5cdb4c7bf44683ab85741a24a3a71a
Rasim ALTUNTAŞ <rasim.altuntas@amperino.com>
2025-08-23 15:28:53 +0300

    Added Product Entity

---
+++ app/src/main/java/com/example/e_commerce/data/local/entity/ProductEntity.kt
@@ -0,0 +1,13 @@
+package com.example.e_commerce.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("products")
+data class ProductEntity(
+    @PrimaryKey
+    val id: String,
+    val name: String,
+    val price: String,
+    val count: String,
+)
